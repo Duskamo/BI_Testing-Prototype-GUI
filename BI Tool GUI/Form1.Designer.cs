83bcce6123fe703fa64e54e0bf8eef2b82a0e72f
Duskamo <dyanownz@yahoo.com>
2017-02-09 09:05:54 -0600

    initial commit

---
+++ BI Tool GUI/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BI_Tool_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tvProject = new System.Windows.Forms.TreeView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvOutput = new System.Windows.Forms.DataGridView();
+            this.mbutCompare = new MetroFramework.Controls.MetroButton();
+            this.mbutExport = new MetroFramework.Controls.MetroButton();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblProjectTitle = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeProjectToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mlProjectTitle = new MetroFramework.Controls.MetroLabel();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.tvProject);
+            this.panel1.Location = new System.Drawing.Point(0, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 553);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // tvProject
+            // 
+            this.tvProject.BackColor = System.Drawing.Color.Red;
+            this.tvProject.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tvProject.Location = new System.Drawing.Point(23, 16);
+            this.tvProject.Name = "tvProject";
+            this.tvProject.Size = new System.Drawing.Size(214, 316);
+            this.tvProject.TabIndex = 0;
+            this.tvProject.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvProject_NodeMouseClick);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(260, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(774, 332);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(232, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 107);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Controls.Add(this.dgvOutput);
+            this.panel3.Location = new System.Drawing.Point(260, 403);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(774, 186);
+            this.panel3.TabIndex = 2;
+            // 
+            // dgvOutput
+            // 
+            this.dgvOutput.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOutput.Location = new System.Drawing.Point(0, 0);
+            this.dgvOutput.Name = "dgvOutput";
+            this.dgvOutput.Size = new System.Drawing.Size(774, 186);
+            this.dgvOutput.TabIndex = 0;
+            // 
+            // mbutCompare
+            // 
+            this.mbutCompare.Location = new System.Drawing.Point(280, 374);
+            this.mbutCompare.Name = "mbutCompare";
+            this.mbutCompare.Size = new System.Drawing.Size(75, 23);
+            this.mbutCompare.TabIndex = 8;
+            this.mbutCompare.Text = "Compare";
+            // 
+            // mbutExport
+            // 
+            this.mbutExport.Location = new System.Drawing.Point(377, 374);
+            this.mbutExport.Name = "mbutExport";
+            this.mbutExport.Size = new System.Drawing.Size(75, 23);
+            this.mbutExport.TabIndex = 9;
+            this.mbutExport.Text = "Export";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // closeProjectToolStripMenuItem
+            // 
+            this.closeProjectToolStripMenuItem.Name = "closeProjectToolStripMenuItem";
+            this.closeProjectToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.lblProjectTitle);
+            this.panel4.Controls.Add(this.menuStrip1);
+            this.panel4.Controls.Add(this.mlProjectTitle);
+            this.panel4.Location = new System.Drawing.Point(0, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(956, 31);
+            this.panel4.TabIndex = 10;
+            // 
+            // lblProjectTitle
+            // 
+            this.lblProjectTitle.AutoSize = true;
+            this.lblProjectTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblProjectTitle.Location = new System.Drawing.Point(277, 10);
+            this.lblProjectTitle.Name = "lblProjectTitle";
+            this.lblProjectTitle.Size = new System.Drawing.Size(0, 13);
+            this.lblProjectTitle.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(956, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem1,
+            this.openProjectToolStripMenuItem,
+            this.closeProjectToolStripMenuItem1});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(37, 27);
+            this.fileToolStripMenuItem1.Text = "File";
+            // 
+            // newProjectToolStripMenuItem1
+            // 
+            this.newProjectToolStripMenuItem1.Name = "newProjectToolStripMenuItem1";
+            this.newProjectToolStripMenuItem1.Size = new System.Drawing.Size(143, 22);
+            this.newProjectToolStripMenuItem1.Text = "New Project";
+            this.newProjectToolStripMenuItem1.Click += new System.EventHandler(this.newProjectToolStripMenuItem1_Click);
+            // 
+            // openProjectToolStripMenuItem
+            // 
+            this.openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
+            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.openProjectToolStripMenuItem.Text = "Open Project";
+            this.openProjectToolStripMenuItem.Click += new System.EventHandler(this.openProjectToolStripMenuItem_Click);
+            // 
+            // closeProjectToolStripMenuItem1
+            // 
+            this.closeProjectToolStripMenuItem1.Name = "closeProjectToolStripMenuItem1";
+            this.closeProjectToolStripMenuItem1.Size = new System.Drawing.Size(143, 22);
+            this.closeProjectToolStripMenuItem1.Text = "Close Project";
+            // 
+            // mlProjectTitle
+            // 
+            this.mlProjectTitle.AutoSize = true;
+            this.mlProjectTitle.Location = new System.Drawing.Point(280, 4);
+            this.mlProjectTitle.Name = "mlProjectTitle";
+            this.mlProjectTitle.Size = new System.Drawing.Size(0, 0);
+            this.mlProjectTitle.TabIndex = 1;
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 590);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.mbutExport);
+            this.Controls.Add(this.mbutCompare);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private MetroFramework.Controls.MetroButton mbutCompare;
+        private MetroFramework.Controls.MetroButton mbutExport;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeProjectToolStripMenuItem;
+        private System.Windows.Forms.TreeView tvProject;
+        private System.Windows.Forms.DataGridView dgvOutput;
+        private System.Windows.Forms.Panel panel4;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel mlProjectTitle;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeProjectToolStripMenuItem1;
+        private System.Windows.Forms.Label lblProjectTitle;
+    }
+}
+
---
+++ BI Tool GUI/Form1.cs
@@ -0,0 +1,206 @@
+﻿using BI_Tool_GUI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BI_Tool_GUI
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        string clickedNode;
+        MenuItem miTestGroupNewTest = new MenuItem("New Test");
+        MenuItem miTestGroupRenameGroup = new MenuItem("Rename Test Group");
+        MenuItem miTestGroupDeleteGroup = new MenuItem("Delete Test Group");
+        MenuItem miTestMapColumns = new MenuItem("Map Columns");
+        MenuItem miTestDeleteTest = new MenuItem("Delete Test");
+        MenuItem miNewTGNewGroup = new MenuItem("New Test Group");
+
+        ContextMenu mnuTestGroup = new ContextMenu();
+        ContextMenu mnuTest = new ContextMenu();
+        ContextMenu mnuNewTG = new ContextMenu();
+
+        #region Init
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTreeViewSelection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(255, 232, 232);
+        }
+
+        private void InitializeTreeViewSelection()
+        {
+            InitializeTestGroupMenuSelection();
+            InitializeTestMenuSelection();
+            InitializePanelMenuSelection();
+        }
+
+        private void InitializePanelMenuSelection()
+        {
+            mnuNewTG.MenuItems.Add(miNewTGNewGroup);
+            miNewTGNewGroup.Click += new EventHandler(miNewTGNewGroup_Click);
+        }
+
+        private void InitializeTestGroupMenuSelection()
+        {
+            mnuTestGroup.MenuItems.Add(miTestGroupNewTest);
+            miTestGroupNewTest.Click += new EventHandler(miTestGroupNewTest_Click);
+
+            mnuTestGroup.MenuItems.Add(miTestGroupRenameGroup);
+            miTestGroupRenameGroup.Click += new EventHandler(miTestGroupRenameGroup_Click);
+
+            mnuTestGroup.MenuItems.Add(miTestGroupDeleteGroup);
+            miTestGroupDeleteGroup.Click += new EventHandler(miTestGroupDeleteGroup_Click);
+        }
+
+        private void InitializeTestMenuSelection()
+        {
+            mnuTest.MenuItems.Add(miTestMapColumns);
+            miTestMapColumns.Click += new EventHandler(miTestMapColumns_Click);
+
+            mnuTest.MenuItems.Add(miTestDeleteTest);
+            miTestDeleteTest.Click += new EventHandler(miTestDeleteTest_Click);
+        }
+
+        
+        #endregion
+
+        #region GUI
+        private void AddSampleProjectGui()
+        {
+            lblProjectTitle.Text = "Compass DB Testing";
+
+            TreeNode table1 = new TreeNode("sql.Negative -> gbq.Negative");
+            TreeNode table2 = new TreeNode("sql.NegativeID -> gbq.NegativeID");
+            TreeNode table3 = new TreeNode("sql.NegativeRowCount -> gbq.NegativeRowCount");
+            TreeNode table4 = new TreeNode("sql.NegativeSchema -> gbq.NegativeSchema");
+            TreeNode table5 = new TreeNode("sql.Positive -> gbq.Positive");
+            TreeNode[] array = new TreeNode[] { table1, table2, table3, table4, table5 };
+
+            TreeNode treeNode = new TreeNode("Full Regression", array);
+            tvProject.Nodes.Add(treeNode);
+
+            array = new TreeNode[] { (TreeNode)table1.Clone(), (TreeNode)table2.Clone(), (TreeNode)table3.Clone()};
+            treeNode = new TreeNode("UPI Regression", array);
+            tvProject.Nodes.Add(treeNode);
+
+            array = new TreeNode[] { (TreeNode)table4.Clone(), (TreeNode)table5.Clone() };
+            treeNode = new TreeNode("UCH Regression", array);
+            tvProject.Nodes.Add(treeNode);
+
+            array = new TreeNode[] { (TreeNode)table1.Clone() };
+            treeNode = new TreeNode("Bug Verification", array);
+            tvProject.Nodes.Add(treeNode);
+        }
+
+        private void OpenProjectFile()
+        {
+            var openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size);
+            Console.WriteLine(result);
+        }
+        #endregion
+
+        #region Controls
+        private void newProjectToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string promptValue = Prompt.ShowDialog("Enter Project Name", "New Project");
+            lblProjectTitle.Text = promptValue;
+        }
+
+        private void openProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenProjectFile();
+            AddSampleProjectGui();
+            
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                mnuNewTG.Show(tvProject, e.Location);
+            }
+        }
+
+        private void tvProject_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                clickedNode = e.Node.Name;
+
+                if (e.Node.Level == 0)
+                {
+                    mnuTestGroup.Show(tvProject, e.Location);
+                } else
+                {
+                    mnuTest.Show(tvProject, e.Location);
+                }
+            }
+        }
+
+        private void miTestGroupNewTest_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form();
+            frm.Text = clickedNode;
+            frm.ShowDialog(this);
+            clickedNode = "";
+        }
+
+        private void miTestGroupRenameGroup_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void miTestGroupDeleteGroup_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void miTestDeleteTest_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void miTestMapColumns_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void miNewTGNewGroup_Click(object sender, EventArgs e)
+        {
+            string promptValue = Prompt.ShowDialog("Enter Test Group Name", "New Test Group");
+            
+            TreeNode treeNode = new TreeNode(promptValue);
+            tvProject.Nodes.Add(treeNode);
+        }
+        #endregion
+
+
+    }
+}
